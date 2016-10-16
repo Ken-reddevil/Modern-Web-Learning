20dfd2227b72667acfc68cc8603abc05f87d1550
Ken <741875304@qq.com>
2016-10-16 12:10:45 +0800

    update homework5

--- homework5/mole/javascript/mole.js
+++ homework5/mole/javascript/mole.js
@@ -4,17 +4,20 @@ var timer = null;
 var status = 0;
 var button;
 
+//add button to the screen
 function addButton() {
     var box = document.getElementById("box");
     for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 6; j++) {
-            var newButton = document.createElement("button");
+            var newButton = document.createElement("input");
+            newButton.type = "radio";
             newButton.className = "button";
+            newButton.checked = false;
             newButton.addEventListener("click", check);
             box.appendChild(newButton);
         }
     }
-    button = document.getElementsByClassName("button");
+    button = document.getElementsByTagName("input");
 }
 
 function startGame() {
@@ -29,15 +32,19 @@ function st() {
     document.getElementById("status").innerHTML = "Gaming";
 }
 
+//action when pressing the start|stop button
 function start() {
+    //if the game is not started, start gaming
     if (status == 0) {
         time = 30;
         score = 0;
         st();
         mouseSet();
     } else if (status == -1) {
+        //if the game is pausing, restart the game
         st();
     } else if (status == 1) {
+        //if gaming, pause the game
         status = -1;
         clearInterval(timer);
         timer = null;
@@ -49,14 +56,17 @@ function timeSet() {
     timer = setInterval(sub, 1000);
 }
 
+//time function
 function sub() {
     time--;
     document.getElementById("time").innerHTML = time;
     if (time == 0) {
         document.getElementById("time").innerHTML = "0";
         document.getElementById("status").innerHTML = "Pressing button to restart";
-        if (document.getElementsByClassName("mouse").length != 0) {
-            document.getElementsByClassName("mouse")[0].className = "button";
+        var mouse = document.getElementsByClassName("mouse");
+        if (mouse.length != 0) {
+            mouse[0].checked = 0;
+            mouse[0].className = "button";
         }
         alert("Game Over, you have got " + score);
         clearInterval(timer);
@@ -65,23 +75,33 @@ function sub() {
     }
 }
 
+//set the mouse hole
 function mouseSet() {
-    if (document.getElementsByClassName("mouse").length == 0) {
-        var i = Math.floor(Math.random() * 61);
-        button[i].className = "mouse";
-    }
+    var i = Math.floor(Math.random() * 61);
+    button[i].className = "mouse";
+    button[i].checked = true;
 }
 
+//action when the button is clicked
 function check(event) {
+    //gaming
     if (status == 1) {
+        //if click the mouse, add 1 to score
         if (event.target.className == "mouse") {
             score++;
             event.target.className = "button";
+            event.target.checked = false;
             mouseSet();
         } else {
+            //if click the empty hole, minus 1 to score
+            event.target.checked = false;
             score--;
         }
         document.getElementById("score").innerHTML = score;
+    }
+    //if not gaming, the buttons' checked are always false
+    if (status == 0 || status == -1) {
+        if (event.target.checked == true && event.target.className == "button") event.target.checked = false;
     }
 }
 
